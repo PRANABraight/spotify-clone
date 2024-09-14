87412ae43cf68a27d9c45bf71f8879a5d79ad189
PRANAB RAI <pranab.rai@mca.christuniversity.in>
2024-09-14 18:43:07 +0530

    first commit

---
+++ SPOTIFY/src/assets/assets.js
@@ -0,0 +1,97 @@
+import backwardIcon from './backward_icon.png';
+import card1Img from './card1img.jpeg';
+import card2Img from './card2img.jpeg';
+import card3Img from './card3img.jpeg';
+import card4Img from './card4img.jpeg';
+import card5Img from './card5img.jpeg';
+import card6Img from './card6img.jpeg';
+import forwardIcon from './forward_icon.png';
+import libraryIcon from './library_icon.png';
+import logo from './logo.png';
+import playMusicBar from './play_musicbar.png';
+import playerIcon1 from './player_icon1.png';
+import playerIcon2 from './player_icon2.png';
+import playerIcon3 from './player_icon3.png';
+import playerIcon4 from './player_icon4.png';
+import playerIcon5 from './player_icon5.png';
+
+export const assets ={
+    backwardIcon,
+    card1Img,
+    card2Img,
+    card3Img,
+    card4Img,
+    card5Img,
+    card6Img,
+    forwardIcon,
+    libraryIcon,
+    logo,
+    playMusicBar,
+    playerIcon2,
+    playerIcon1,
+    playerIcon3,
+    playerIcon4,
+    playerIcon5
+
+}
+
+export const albumsData = [{
+    id:0,
+    name: "Top 50 Global" ,
+    image: card1Img,
+    desc: "You weekly update of the most played tracks",
+    bgColor: "#2a4365"
+
+},
+{
+    id:1,
+    name: "Top 50 India",
+    image: card6Img,
+    desc:"Your weekly update of most played tracks",
+    bgColor: "22543d"
+
+},
+{
+    id:2,
+    name: "Top songs Global",
+    image:card5Img,
+    desc: "Your weekly update of most played tracks",
+    bgColor: "742a2a"
+
+},
+
+]
+
+// ##11.50 yt
+
+// export const songsData = [
+//     {
+//     id:0,
+//     name: "Song One",
+//     image:card2Img,
+//     file: song1.mp3, //mp3 file,
+//     desc: "Put a smile on your face with these happy tunes",
+//     duration: "3:00"
+        
+//     },
+//     {id:1,
+//     name: "Song Two",
+//     image:card3Img,
+//     desc: "Put a smile on your face with these happy tunes",
+//     duration: "3:00"
+// },
+
+//      {id:1,
+//     name: "Song Three",
+//     image:card3Img,
+//     desc: "Put a smile on your face with these happy tunes",
+//     duration: "3:00"
+// },
+
+//     {id:1,
+//     name: "Song Four",
+//     image:card3Img,
+//     desc: "Put a smile on your face with these happy tunes",
+//     duration: "3:00"
+// }
+// ]
